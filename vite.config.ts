29db9c558b5b5040e5789891767750fb859e4ee9
João Alóca <joao.aloca@outlook.com>
2025-04-24 19:47:00 -0300

    tailwind and shadcn-vue install and configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { fileURLToPath, URL } from 'node:url'
 import VueRouter from 'unplugin-vue-router/vite'
 
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -8,6 +11,11 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [VueRouter(), vue(), vueDevTools()],
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer],
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
