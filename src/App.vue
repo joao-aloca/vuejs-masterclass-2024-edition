4646cb915ec60e96a59251c164d1668ac4623a32
João Aloca <joao.aloca@andradas.br>
2025-05-15 15:29:06 -0300

    checagem de sessão em cada página navegada

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <script setup lang="ts">
-import { useErrorStore } from './stores/error';
+import { useErrorStore } from './stores/error'
 
 const errorStore = useErrorStore()
 
 onErrorCaptured((error) => {
   errorStore.setError({ error })
 })
+
+onMounted(() => {})
 </script>
 
 <template>
   <AuthLayout>
     <AppErrorPage v-if="errorStore.activeError" />
-
     <RouterView v-else v-slot="{ Component, route }">
       <Suspense v-if="Component" :timeout="0">
         <Component :is="Component" :key="route.name"></Component>
-
         <template #fallback>
           <span>Loading ..</span>
         </template>
