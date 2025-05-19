b81e548fd71b685b8f8176db03fd32660823cdb3
João Alóca <joao.aloca@outlook.com>
2025-05-19 07:01:44 -0300

    botão de sign out (lógica ainda não implementada)

--- src/components/Layout/SidebarLinks.vue
+++ src/components/Layout/SidebarLinks.vue
@@ -2,7 +2,7 @@
 
 interface LinkProp {
   title: string,
-  to: string,
+  to?: string,
   icon: string
 }
 
@@ -12,9 +12,19 @@ defineProps<{
 </script>
 
 <template>
-  <RouterLink activeClass="text-primary bg-muted" v-for="link in links" :key="link.title" :to="link.to"
-    class="flex items-center gap-3 px-4 py-2 mx-2 transition-colors rounded-lg hover:text-primary justify-center lg:justify-normal text-muted-foreground">
-    <iconify-icon :icon="link.icon"></iconify-icon>
-    <span class="hidden lg:block text-nowrap">{{ link.title }}</span>
-  </RouterLink>
+  <template v-for="link in links" :key="link.title">
+    <RouterLink v-if="link.to" activeClass="text-primary bg-muted" :to="link.to"
+      class="flex items-center gap-3 px-4 py-2 mx-2 transition-colors rounded-lg hover:text-primary justify-center lg:justify-normal text-muted-foreground">
+      <iconify-icon :icon="link.icon"></iconify-icon>
+      <span class="hidden lg:block text-nowrap">{{ link.title }}</span>
+    </RouterLink>
+
+    <div v-else
+      class="flex items-center gap-3 px-4 py-2 mx-2 transition-colors rounded-lg hover:text-primary justify-center lg:justify-normal text-muted-foreground">
+      <iconify-icon :icon="link.icon"></iconify-icon>
+      <span class="hidden lg:block text-nowrap">{{ link.title }}</span>
+    </div>
+
+  </template>
+
 </template>
