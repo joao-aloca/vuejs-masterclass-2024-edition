2dbafe1db5b98c28734d8507a40b01d365969704
João Aloca <joao.aloca@andradas.br>
2025-05-20 11:20:42 -0300

    validação de email e senha em tempo real na tela de login

--- src/composables/formErrors.ts
+++ src/composables/formErrors.ts
@@ -1,15 +1,38 @@
+import type { LoginForm } from '@/types/AuthForm'
 import type { AuthError } from '@supabase/supabase-js'
+
+type FormErrors<T> = {
+  [K in keyof T]: string[]
+}
 
 export const useFormErrors = () => {
   const serverError = ref('')
+  const realtimeErrors = ref<FormErrors<LoginForm>>()
 
   const handleServerError = (error: AuthError) => {
     serverError.value =
       error.message === 'Invalid login credentials' ? 'Email e/ou senha incorretos' : error.message
   }
 
+  const handleLoginForm = async (formData: LoginForm) => {
+    realtimeErrors.value = {
+      email: [],
+      password: [],
+    }
+
+    const { validadeEmail, validatePassword } = await import('@/utils/formValidations')
+
+    const emailErrors: string[] = validadeEmail(formData.email)
+    if (emailErrors.length) realtimeErrors.value.email = emailErrors
+
+    const passwordErrors: string[] = validatePassword(formData.password)
+    if (passwordErrors.length) realtimeErrors.value.password = passwordErrors
+  }
+
   return {
     serverError,
     handleServerError,
+    realtimeErrors,
+    handleLoginForm,
   }
 }
