e40d2de52529948d388bf4fbbfa4bb553856b57d
João Aloca <joao.aloca@outlook.com>
2025-05-22 18:48:17 -0300

    atualização do status do projeto no db e animação da opção de alterar status

--- src/components/AppInPlaceEdit/AppInPlaceEditStatus.vue
+++ src/components/AppInPlaceEdit/AppInPlaceEditStatus.vue
@@ -1,11 +1,32 @@
 <script setup lang="ts">
 const status = defineModel<'in-progress' | 'completed'>()
-const isCompleted = status.value === 'completed'
+const isCompleted = ref<boolean>(status.value === 'completed')
+const emit = defineEmits(['commit'])
+
+const toggleValue = () => {
+  status.value = isCompleted.value ? 'in-progress' : 'completed'
+  isCompleted.value = !isCompleted.value
+  emit('commit')
+}
 </script>
 
 <template>
-  <div class="text-2xl cursor-pointer">
-    <iconify-icon v-if="isCompleted" icon="lucide:circle-check" class="text-green-500" />
-    <iconify-icon v-else icon="lucide:circle-dot" class="text-gray-500" />
+  <div class="text-2xl cursor-pointer" @click="toggleValue()">
+    <Transition mode="out-in">
+      <iconify-icon v-if="isCompleted" icon="lucide:circle-check" class="text-green-500" />
+      <iconify-icon v-else icon="lucide:clock" class="text-gray-500" />
+    </Transition>
   </div>
 </template>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: transform 0.1s;
+}
+
+.v-enter-from,
+.v-leave-to {
+  transform: scale(0.3);
+}
+</style>
